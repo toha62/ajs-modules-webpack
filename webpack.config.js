08bbb3ca1ec5acc72e791196e13be3c532f16617
Anton Trifonov <toha62@inbox.ru>
2023-01-23 11:16:04 +0300

    add babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,13 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
         test: /\.css$/i,
         use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
